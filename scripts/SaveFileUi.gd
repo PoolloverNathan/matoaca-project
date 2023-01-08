6bdb42b802e9b8f9535013dcba7caef6045b2991
PoolloverNathan <nathan.kulzer@protonmail.com>
2023-01-08 03:38:05 -0500

    commit

--- scripts/SaveFileUi.gd
+++ scripts/SaveFileUi.gd
@@ -1,21 +1,26 @@
-tool extends PanelContainer
+tool extends Button
 export var filenum = 0
 var _File = File.new()
+onready var header := $Panel/Box/Header
+onready var contents := $Panel/Box/Contents
+onready var buttons := $Panel/Box/Buttons
 
 func _process(_delta):
 	var file = "user://save_%s.tscn"
-	$Control/Header.text = "File " + str(filenum)
+	header.text = "File " + str(filenum)
 	if _File.file_exists(file):
 		var scn = load("user://save_%s.tscn" % str(filenum))
 		var green = PlayerService.getGreenGemCount(scn)
 		var orange = PlayerService.getOrangeGemCount(scn)
-		$Control/Label.text = """
+		contents.text = """
 		a
 		b
 		c
 		"""
 	else:
-		$Control/Label.text = ""
+		contents.text = "This slot is empty."
+		buttons.get_no
+		
 
 func _protected(_value):
 	assert(false, "Cannot set read-only property")
