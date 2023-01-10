75e163c98a88d26e806f1d8371bf0da12ab97a81
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2023-01-10 07:39:02 -0500

    Fix life being illegal

--- scripts/Savegame.gd
+++ scripts/Savegame.gd
@@ -1,5 +1,5 @@
 class_name Savegame extends Node
-export var spawn_at := Vector2(112, -360)
+export var spawn_at := Vector2(24, -520)
 export var level := "res://levels/level2.tscn"
 export var current_stage := 1
 export var started_at: int = OS.get_unix_time()
