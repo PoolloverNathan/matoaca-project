94154071fdb46a6548f4ea36fa48f193737fceba
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2023-01-12 11:56:20 -0500

    keys

--- scripts/Preferences.gd
+++ scripts/Preferences.gd
@@ -1,8 +1,8 @@
 class_name Preferences extends Node
 
-export var move_left := KEY_4
-export var move_right := KEY_1
-export var move_up := KEY_3
-export var move_down := KEY_7
-export var jump := KEY_5
+export var move_left := KEY_T
+export var move_right := KEY_7
+export var move_up := KEY_B
+export var move_down := KEY_E
+export var jump := KEY_ENTER
 export var cling_wall = KEY_MINUS
